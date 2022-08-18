ac92cb5a8b7177b5a6c427b24a54935303787860
Aditya Kesuma <aditezo24@gmail.com>
2022-08-18 16:18:46 +0700

    DOM Test 2

--- Latihan5-DOM Test/script.js
+++ Latihan5-DOM Test/script.js
@@ -6,6 +6,7 @@ const redButton = sectionA.querySelector(".redButton");
 const yellowButton = sectionA.querySelector(".yellowButton");
 const blueButton = sectionA.querySelector(".blueButton");
 const greenButton = sectionA.querySelector(".greenButton");
+const acakButton = sectionA.querySelector(".acakButton");
 
 const color = ["white", "red", "Yellow", "Blue", "Green"];
 
@@ -40,3 +41,56 @@ greenButton.addEventListener("click", function() {
     headerText.classList.replace("blackText", "whiteText");
     alert("Background changed to " + color[4]);
 });
+
+acakButton.addEventListener("click", warnaAcak);
+
+
+// ---------------------------------------
+function red() {
+    document.body.style.backgroundColor = "red";
+    headerText.classList.replace("blackText", "whiteText");
+    alert("Background changed to " + color[1]);
+}
+
+function yellow() {
+    document.body.style.backgroundColor = "yellowgreen";
+    headerText.classList.replace("blackText", "whiteText");
+    alert("Background changed to " + color[2]);
+}
+
+function blue() {
+     document.body.style.backgroundColor = "lightskyblue";
+    headerText.classList.replace("blackText", "whiteText");
+    alert("Background changed to " + color[3]);
+}
+
+function green(){
+      document.body.style.backgroundColor = "green";
+    headerText.classList.replace("blackText", "whiteText");
+    alert("Background changed to " + color[4]);
+}
+
+
+var a = 0;
+
+function warnaAcak() {
+
+    
+    a = Math.floor(Math.random() * 4);
+    alert(a);
+    if(a == 0){
+        red();
+    }if(a == 1){
+        yellow();
+    }if(a == 2){
+        blue();
+    }if(a == 3){
+        green();
+    }
+}
+
+
+
+
+
+
