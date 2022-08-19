96008e4779c193734dbc49c98145b022b2cf19ba
Aditya Kesuma <aditezo24@gmail.com>
2022-08-19 21:21:07 +0700

    DOM traversal

---
+++ Latihan6-DOM Traversal/script.js
@@ -0,0 +1,13 @@
+const close = document.querySelectorAll(".close");
+
+for(let i = 0; i < close.length; i++){
+    close[i].addEventListener("click", function(e){
+    close[i].parentElement.style.display = "none";
+});
+}
+
+// Note :
+// Element.parentElement <-
+// parentElement adalah fungsi untuk mengambil parent dari suatu Element
+// ketika sudah di select maka kita dapat mengaktifkan display = "none"
+// agar Pop-Up menu hilang. (Teknik untuk Pop-up menu).
