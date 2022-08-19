34b075e9aacb0485c30a3ca1e556748bba60fd9c
Aditya Kesuma <aditezo24@gmail.com>
2022-08-19 23:28:16 +0700

    Image Gallery DOM

---
+++ Latihan9-Image Gallery (Latihan)/script.js
@@ -0,0 +1,21 @@
+const img = document.querySelectorAll(".item");
+const heroImg = document.querySelector(".hero img");
+const imgText = document.querySelector(".textJudul h1");
+
+img.forEach(function (e, i){
+    e.addEventListener("mouseenter",function(x){
+        x.target.style.opacity = 0.5;
+        x.target.style.cursor = "pointer";
+        e.addEventListener("click",function(){
+            heroImg.setAttribute("src", x.target.src);
+            imgText.innerHTML = "Image " + (i+1);
+        })
+    });
+});
+
+img.forEach(function (e){
+    e.addEventListener("mouseleave",function(x){
+        x.target.style.opacity = 1;
+    });
+});
+
